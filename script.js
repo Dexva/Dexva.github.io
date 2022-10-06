e13f26d91a1b6782a77169e488168c9c1fc41bbb
Davis Magpantay <magpantay_d@yahoo.com>
2022-10-06 09:19:34 +0800

    added rotating stuff

--- script.js
+++ script.js
@@ -28,13 +28,14 @@ function onResults(results) {
     canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
     canvasCtx.drawImage(results.image, 0, 0, canvasElement.width, canvasElement.height);
     if (results.multiHandLandmarks) {
+        // console.log(results.multiHandLandmarks);
         for (const landmarks of results.multiHandLandmarks) {
-            var pointer_x = landmarks[8].x;
-            var pointer_y = landmarks[8].y;
-            var pointer_z = landmarks[8].z;
+
+            // console.log(landmarks);
             
             //console.log(pointer_x, pointer_y, pointer_z);
-            drawOnScreen(pointer_x,pointer_y,pointer_z);
+            drawBeaker(landmarks);
+            // drawOnScreen(pointer_x,pointer_y,pointer_z);
 
             drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS,
                             {color: '#00FF00', lineWidth: 3});
@@ -57,6 +58,60 @@ const camera = new Camera(videoElement, {
 camera.start();
 
 let lastX=0, lastY=0;
+
+function drawBeaker (landmarks) {
+    let drawCanv = document.getElementById('drawing_canvas');
+    drawCanv.width = document.getElementsByClassName('drawing_container')[0].clientWidth;
+    drawCanv.height = document.getElementsByClassName('drawing_container')[0].clientHeight;
+    const drawCtx = drawCanv.getContext('2d');
+
+    let pointer = {x: landmarks[9].x, y: landmarks[9].y};
+    let node1 = {x: landmarks[5].x, y: landmarks[5].y}; //pointer node
+    let node2 = {x: landmarks[17].x, y: landmarks[17].y}; //pinky node
+
+    // rotation setting
+    let degrees = 0;
+    let nodeDX = node1.x - node2.x;
+    let nodeDY = node1.y - node2.y;
+
+    console.log(`nodeDX = ${nodeDX}, nodeDY = ${nodeDY}`);
+
+    let nodeDistance = ((nodeDX)**2 + (nodeDY)**2)**0.5;
+    let initialDegree = Math.asin(nodeDX/nodeDistance);
+    if (nodeDY > 0) {
+        degrees = initialDegree;
+    }
+    else if (nodeDY == 0) {
+        degrees = 0;
+    }
+    else if (nodeDY < 0) {
+        degrees = initialDegree;
+    } else{}
+
+    let beaker = {x:0, y:pointer.y};
+    beaker.x = 0.5 - (pointer.x-0.5);
+    beaker.x *= drawCanv.width;
+    beaker.y *= drawCanv.height;
+
+    drawCtx.save();
+    let startX = beaker.x-10;
+    let startY = beaker.y-10;
+    let width = 30, height = 90;
+
+    drawCtx.translate(startX + width/2, startY + height/2);
+    drawCtx.rotate(-(degrees*180)*Math.PI/180);
+    drawCtx.rect(-width/2,-height/2,width,height);
+    drawCtx.fill();
+
+    drawCtx.restore();
+    
+    console.log(degrees);
+    //console.log(`${pointer.x}, ${pointer.y}`);
+    //console.log(`${node1.x}, ${node1.y}`);
+    //console.log(`${node2.x}, ${node2.y}`); //need to .x or .y to console log the values
+}
+
+
 function drawOnScreen(x, y, z) { //for canvases, (0,0) is top left corner
     let drawingCanvas = document.getElementById('drawing_canvas');
     drawingCanvas.width = document.getElementsByClassName('drawing_container')[0].clientWidth;
@@ -69,7 +124,9 @@ function drawOnScreen(x, y, z) { //for canvases, (0,0) is top left corner
     let rX = x*drawingCanvas.width;
     let rY = y*drawingCanvas.height;
     
-    console.log(rX, rY);
+    // console.log(rX, rY);
+
+    //Processing degrees
 
     /*
     drawingCtx.beginPath();
